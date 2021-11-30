6fa48e043160e7d87986beb411c7bfe6b44476dc
Paris Mollo <parismollochristondis@gmail.com>
2021-11-30 17:31:51 +0100

    (controleur) mis a jour de couleur

--- Controleur.java
+++ Controleur.java
@@ -1,4 +1,11 @@
+import java.awt.Color;
+
 public class Controleur {
     Modele model;
     Vue vue;
+
+    public void sliderMoved() {
+        model.color = new Color(vue.rouge.getValue(), vue.vert.getValue(), vue.bleu.getValue());
+        vue.miseAJour();
+    }
 }
